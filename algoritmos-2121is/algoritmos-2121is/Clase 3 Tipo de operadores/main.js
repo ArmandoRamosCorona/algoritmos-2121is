72bd8169fa6f741191be29d900f7a70827a150c9
Armando Ramos Corona <armando_1321124080@uptecamac.edu.mx>
2021-09-09 13:34:18 -0500

    Clase4

---
+++ algoritmos-2121is/algoritmos-2121is/Clase 3 Tipo de operadores/main.js
@@ -0,0 +1,91 @@
+
+// -- Buenas prácticas de programación
+
+var perro = "firu";
+var perroPradera = "firu";
+var perroPraderaAmericano = "firu";
+
+/*
+    <   menor que
+    >   mayor que
+    =   asignar valor
+    ==  Igual en comparación
+    !=  Diferente
+    <=  Menor o igual
+    >=  Mayor o igual     
+    && Y
+    || O
+    !  Negación    
+*/
+// var x = 300; // Variable para comparar
+// var y = 20;
+// var s = 30;
+// if(x < y){
+//     console.log(x + " es menor a " + y);
+// }else{
+//     console.log(x +" es mayor a " + y);
+// }
+
+// var humano1 = "Emmanuel";
+// var humano2 = "Emmanuel";
+
+// if(humano1 == humano2){
+//     console.log("Son iguales");
+// }else{
+//     console.log("Son diferentes");
+// }
+
+// var pi = 3.1416;
+// var radio = 10;
+
+// if(pi != radio){
+//     console.log("Son diferentes");
+// }else{
+//     console.log("Son iguales");
+// }
+
+
+// var transporte = "carro";
+
+// console.log("Paso 1: Salir a la entrada de mi casa");
+
+// if(transporte == "combi"){
+//     console.log("Paso 2: Subirme a la combi");
+// }else{
+//     console.log("Paso 2: Subirme al transporte publico");
+// }
+
+// console.log("Paso 3: Bajar en paradero de Tecámac");
+// console.log("Paso 4: Caminar a la entrada de la uni");
+// console.log("Paso 5: Caminar al aula");
+
+
+
+var n1 = 10;
+var n2 = 5;
+var operacion = "perro";
+var total = 0;
+
+if(operacion == "suma"){
+    total = n1 + n2;
+}
+
+if(operacion == "resta"){
+    total = n1 - n2;
+}
+
+if(operacion == "multiplicacion"){
+    total = n1 * n2;
+}
+
+if(operacion == "division"){
+    total = n1 / n2;
+}
+
+console.log(total);
+
+
+
+
+
+
---
+++ algoritmos-2121is/algoritmos-2121is/Clase 4 Estrucuras de control/main.js
@@ -0,0 +1,97 @@
+// var n1 = 10;
+// var n2 = 5;
+// var operacion = "perro";
+// var total = 0;
+
+// if(operacion == "suma"){
+//     total = n1 + n2;
+// }
+
+// if(operacion == "resta"){
+//     total = n1 - n2;
+// }
+
+// if(operacion == "multiplicacion"){
+//     total = n1 * n2;
+// }
+
+// if(operacion == "division"){
+//     total = n1 / n2;
+// }
+// console.log(total);
+
+
+// --------------------------------------------------
+// var n1 = 10;
+// var n2 = 5;
+// var operacion1 = "resta";
+// var operacion2 = "division";
+// var total1 = 0;
+// var total2 = 0;
+
+// if(operacion1 == "suma"){
+//     total1 = n1 + n2;
+// }else{
+//     total1 = n1 - n2;
+// }
+
+// if(operacion2 == "division"){
+//     total2 = n1 / n2;
+// }else{
+//     total2 = n1 * n2;
+// }
+
+// console.log("El resultado de la operacion 1 = "+ total1);
+// console.log("El resultado de la operacion 2 = "+ total2);
+
+
+//----------------------------------------------------------------------------------------------------
+// -- Constante
+// const hola = "jeje";
+// console.log(hola);
+// hola = "jejeje";
+// console.log(hola);
+
+// -- Variable
+// var ok = "HOla mundo";
+// console.log(ok);
+// ok = "Saludos desde otra linea de codigo";
+// console.log(ok);
+
+ 
+// -- Variable para caso
+var operacion = "suma";
+var n1 = 10;
+var n2 = 5;
+switch(operacion){
+    case "suma":
+        if(n1 < n2){
+            console.log("Operacion es suma y n1 es menor a n2");
+        }else{
+            console.log("Operacion es suma y n2 es menor a n1");
+        }        
+    break;
+    case "resta":
+        console.log("La operacion es resta");
+    break;
+    case "multiplicacion":
+        console.log("La operacion es multiplicacion");
+    break;
+    case "division":
+        console.log("La operacion es division");
+    break;
+    default:
+        console.log("No se reconoce la operacion");
+    break;
+}
+
+
+
+
+
+
+
+
+
+
+
